23a9d66a1fa994fc1ccbf5875b4cce45603f8a1e
Ziyu Ye <55178744+Zye3@users.noreply.github.com>
2025-02-16 23:03:38 +0000

    Update test.py

--- test.py
+++ test.py
@@ -150,11 +150,8 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Load the model !!! Note: replace the mode_type in the config,py first
-    # model_path = f'runs/HPM_UNet_64_0.001_48_90_0.2&0.1_2024-08-27__16-15-48/best_model.pth'
-    model_path = (f'runs/'
-                  f'Multi_64_64_0.2_0.1/'
-                  f'Att_UNet_64_0.001_48_90_0.1&0.2_2024-08-15__21-28-29/'
-                  f'{args.model_type}.pth')
+    # model_path = f''
+    model_path = (f'')
     model = load_model(model_path, args.model_type, device)
 
     # load log_dir
